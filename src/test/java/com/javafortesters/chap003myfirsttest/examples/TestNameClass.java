cdf66365d62c1c94b2869b74c1e6df31ab0b9609
titar <dmitry.titar@ligastavok.ru>
2018-11-07 18:15:10 +0300

    chapter3 done

---
+++ src/test/java/com/javafortesters/chap003myfirsttest/examples/TestNameClass.java
@@ -0,0 +1,13 @@
+package com.javafortesters.chap003myfirsttest.examples;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TestNameClass {
+
+    @Test
+    public void falseMethod() {
+        fail();
+    }
+}
